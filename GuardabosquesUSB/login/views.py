fc7a781aba14ac5ac3446ceaa9e455e17de5f204
danielar92 <danielar92@gmail.com>
2014-05-28 17:34:50 -0430

    Me lleva.

--- GuardabosquesUSB/login/views.py
+++ GuardabosquesUSB/login/views.py
@@ -1,6 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
+from django.forms.models import modelformset_factory
+from django.template import RequestContext
+
+from login.models import Persona
 
 # Create your views here.
 
 def login(request):
     return render (request, 'login.html')
+
+
+# def add_(request):
+#     InvitadoFormSet = modelformset_factory(Invitado)
+#     if request.method == 'POST':
+#         formset = InvitadoFormSet(request.POST, request.FILES)
+#         if formset.is_valid():
+#             formset.save()
+#             return consultar_invitados(request)
+#     else:
+#         formset = InvitadoFormSet(queryset=Invitado.objects.none())
+
+#     return render(request, "agregar/agregarInvitado.html", {
+#         "formset": formset,
+#     })
