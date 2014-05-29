ba9feaf52e266929f9a1f438c18f20d374fe2c67
Patrick <patrick.rengifo@gmail.com>
2014-05-29 09:21:20 -0430

    perol de estudiante

--- GuardabosquesUSB/login/models.py
+++ GuardabosquesUSB/login/models.py
@@ -1,14 +1,38 @@
+from django.contrib.auth.models import User
 from django.db import models
-from django import forms
 
-class Persona(models.Model):
+CARRERA_CHOICES = (
+    (0, 'Arquitectura'),
+    (1, 'Ing. Computacion'),
+    (2, 'Ing. Electrica'),
+    (3, 'Ing. Electronica'),
+    (4, 'Ing. Geofisica'),
+    (5, 'Ing. Mantenimiento'),
+    (6, 'Ing. Materiales'),
+    (7, 'Ing. Mecanica'),
+    (8, 'Ing. Produccion'),
+    (9, 'Ing. Telecomunicaciones'),
+    (10, 'Ing. Quimica'),
+    (11, 'Lic. Biologia'),
+    (12, 'Lic. Comercio Exterior'),
+    (13, 'Lic. Gestion de la Hospitalidad'),
+    (14, 'Lic. Fisica'),
+    (15, 'Lic. Matematica'),
+    (16, 'Lic. Quimica'),
+    (17, 'Urbanismo'),
+    (18, 'Admin. Aduanera'),
+    (19, 'Admin. Hotelera'),
+    (20, 'Admin. Transporte'),
+    (21, 'Admin. Turismo'),
+    (22, 'Comercio Exterior (Carrera Corta)'),
+    (23, 'Mantenimiento Aeronautica'),
+    (24, 'Organizacion Empresarial'),
+    (25, 'Tec. Electrica'),
+    (26, 'Tec. Electronica'),
+    (27, 'Tec. Mecanica'),
+)
 
-  nombre     = models.CharField(max_length=100)
-  apellido   = models.CharField(max_length=100)
-  carnet     = models.CharField(max_length=8, primary_key=True)
-  email      = models.CharField(max_length=100)
-  carrera    = models.CharField(max_length=100)
-  clave      = models.CharField(max_length=17)
-
-  def __unicode__(self):
-      return self.nombre+' '+self.apellido
+class Estudiante(models.Model):
+  user    = models.OneToOneField(User)
+  carrera = models.IntegerField('carrera', choices=CARRERA_CHOICES)
+  carnet  = models.CharField(max_length=8)
