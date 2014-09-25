8d8512d35ded2d88a66637c583717759861a33df
Patrick Rengifo <patrick.rengifo@gmail.com>
2014-09-25 18:29:46 -0430

    try 18

--- GuardabosquesUSB/GuardabosquesUSB/wsgi.py
+++ GuardabosquesUSB/GuardabosquesUSB/wsgi.py
@@ -32,7 +32,9 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "GuardabosquesUSB.settings.produ
 # from django.core.wsgi import get_wsgi_application
 # application = get_wsgi_application()
 
+from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
+
 application = Cling(get_wsgi_application())
 # Apply WSGI middleware here.
 # from helloworld.wsgi import HelloWorldApplication
