b59174ef80f96c3d1b85280aa37f29305c905206
Patrick Rengifo <patrick.rengifo@gmail.com>
2014-09-25 19:01:07 -0430

    try 24

--- GuardabosquesUSB/GuardabosquesUSB/settings/production.py
+++ GuardabosquesUSB/GuardabosquesUSB/settings/production.py
@@ -66,8 +66,8 @@ CACHES = {
 # Static asset configuration
 import os
 PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = 'staticfiles'
-STATIC_URL = 'GuardabosquesUSB/static/'
+STATIC_ROOT = 'static'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
     os.path.join(PROJECT_PATH, 'static'),
