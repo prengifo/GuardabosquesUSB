071fe34331d763d5810e299e44b5028ab098d44f
Patrick Rengifo <patrick.rengifo@gmail.com>
2014-09-25 18:52:48 -0430

    try 22

--- GuardabosquesUSB/GuardabosquesUSB/settings/production.py
+++ GuardabosquesUSB/GuardabosquesUSB/settings/production.py
@@ -63,13 +63,14 @@ CACHES = {
 }
 ########## END CACHE CONFIGURATION
 
-import dj_database_url
-DATABASES['default'] = dj_database_url.config()
-
-STATIC_URL = '/static/'
+# Static asset configuration
+import os
+PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(PROJECT_PATH, 'static'),
 )
 
 
